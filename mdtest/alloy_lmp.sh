2752e6f347532c034b11d5ef2d402720e8827385
jushinpon <32800219+jushinpon@users.noreply.github.com>
2024-11-20 11:56:06 +0800

    Update alloy_lmp.sh

--- mdtest/alloy_lmp.sh
+++ mdtest/alloy_lmp.sh
@@ -8,9 +8,20 @@
 ##SBATCH --ntasks-per-node=12
 
 hostname
-source /opt/anaconda3/bin/activate deepmd-cpu-v3
-export LD_LIBRARY_PATH=/opt/deepmd-cpu-v3/lib:/opt/deepmd-cpu-v3/lib/deepmd_lmp:$LD_LIBRARY_PATH
-export PATH=/opt/deepmd-cpu-v3/bin:$PATH
+if [ -f /opt/anaconda3/bin/activate ]; then
+    
+    source /opt/anaconda3/bin/activate deepmd-cpu-v3
+    export LD_LIBRARY_PATH=/opt/deepmd-cpu-v3/lib:/opt/deepmd-cpu-v3/lib/deepmd_lmp:\$LD_LIBRARY_PATH
+    export PATH=/opt/deepmd-cpu-v3/bin:\$PATH
+
+elif [ -f /opt/miniconda3/bin/activate ]; then
+    source /opt/miniconda3/bin/activate deepmd-cpu-v3
+    export LD_LIBRARY_PATH=/opt/deepmd-cpu-v3/lib:/opt/deepmd-cpu-v3/lib/deepmd_lmp:\$LD_LIBRARY_PATH
+    export PATH=/opt/deepmd-cpu-v3/bin:\$PATH
+else
+    echo "Error: Neither /opt/anaconda3/bin/activate nor /opt/miniconda3/bin/activate found."
+    exit 1  # Exit the script if neither exists
+fi
 
 node=1
 threads=$(nproc)
